b3e75af74a29f800cee5881300d56a66141410b4
Fenrisulfr <1766123079@qq.com>
2022-11-12 22:12:10 +0800

    Refactor template code

--- functional_test.py
+++ functional_test.py
@@ -28,11 +28,20 @@ class NewVisitorTest(unittest.TestCase):
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)  
-        time.sleep(1)  
+        # 页面中还有一个文本框，可以输入其他的待办事项
+        # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
+        # 伊迪丝做事很有条理
+        inputbox = self.browser.find_element('id', 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+
         table = self.browser.find_element('id','id_list_table')
         rows = table.find_elements('tag name','tr')  
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows), 'Now to-do item did not appear in table'
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+                [row.text for row in rows]
         )
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,3 +3,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/lists/')
         self.assertTemplateUsed(response, 'home.html')
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
