94eecab7812aaac3eac5953e39705afef1626fbc
Fenrisulfr <1766123079@qq.com>
2022-11-12 11:21:41 +0800

    Basic view now returns minimal HTML.

--- functional_test.py
+++ functional_test.py
@@ -7,15 +7,16 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):  
         self.browser.quit()
-        
+
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用的首页
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8000/lists/')
         # 她注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)  
-        self.fail('Finish the test!')  
+        # self.fail('Finish the test!')  
+
         # 应用邀请她输入一个待办事项
-        [其余的注释和之前一样]
+
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
