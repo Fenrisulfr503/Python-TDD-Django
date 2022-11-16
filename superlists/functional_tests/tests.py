e2e5eee0d26bb35b6457287a3a089621b0dc6a48
Fenrisulfr <1766123079@qq.com>
2022-11-16 21:33:18 +0800

    Add domain_name for functional test.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 import unittest
 
 MAX_WAIT = 10
@@ -12,6 +13,9 @@ MAX_WAIT = 10
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
     def tearDown(self):
         self.browser.refresh()
         self.browser.quit()
