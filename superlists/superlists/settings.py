a38667cc2b89445b79ac3839c9dfbfb784c0cd91
Fenrisulfr <1766123079@qq.com>
2022-11-16 23:19:01 +0800

    change basedir setting.

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + '/database/db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, './database/db.sqlite3'),
     }
 }
 
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT= os.path.abspath(os.path.join(BASE_DIR, './static'))
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
